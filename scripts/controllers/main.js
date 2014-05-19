27a5b20eb34a13fbab6fb7ef54f3b8d6ff246632
thechunsik <thechunsik@gmail.com>
2014-05-20 00:28:45 +0900

    test

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -46,18 +46,20 @@ angular.module('wonsoonApp')
 			}
 		};
 
-		var center,
+		var oPolyLine,
+			startMarker,
 			currentMarker,
 			pictures_markers = [],
 			pictures = [],
 			route_positions = [];
 
 		// marker icons
-		var icon = new daum.maps.MarkerImage('https://farm6.staticflickr.com/5515/14181411316_e679cd61f6_o.png', new daum.maps.Size(31, 34));
-		var icon_click = new daum.maps.MarkerImage('https://farm6.staticflickr.com/5568/14017946547_caa4ce9424_o.png', new daum.maps.Size(31, 34));
-		var icon_wonsoon = new daum.maps.MarkerImage('https://farm3.staticflickr.com/2920/14239339603_1bb69d6b0e_o.png', new daum.maps.Size(140, 166));
-		var icon_start = new daum.maps.MarkerImage('https://farm6.staticflickr.com/5079/14022441047_5bcd85a3b1_o.png', new daum.maps.Size(110, 80));
-
+		var oSize = new nhn.api.map.Size(30, 34);
+		var oOffset = new nhn.api.map.Size(15, 17);
+		var icon = new nhn.api.map.Icon('https://farm6.staticflickr.com/5515/14181411316_e679cd61f6_o.png', oSize, oOffset);
+		var icon_click = new nhn.api.map.Icon('https://farm6.staticflickr.com/5568/14017946547_caa4ce9424_o.png', oSize, oOffset);
+		var icon_wonsoon = new nhn.api.map.Icon('https://farm3.staticflickr.com/2936/14206569692_49cd5a620d_o.png', new nhn.api.map.Size(46, 70));
+		var icon_start = new nhn.api.map.Icon('https://farm6.staticflickr.com/5079/14022441047_5bcd85a3b1_o.png', new nhn.api.map.Size(45, 30));
 		// get current activities
 		$.ajax(api.url + api.options.activityInfo.oneDay + date, {
 			method: 'get',
@@ -84,13 +86,15 @@ angular.module('wonsoonApp')
 			success: function(data) {
 				for(var i = 0; i < data.pictures.length; i++) {
 					var picture = {};
-					picture.position = new daum.maps.LatLng(data.pictures[i]['lat'], data.pictures[i]['lng']);
+					picture.position = new nhn.api.map.LatLng(data.pictures[i]['lat'], data.pictures[i]['lng']);
 					picture.url = 'http://121.78.54.210:5018/wonsoon' + data.pictures[i]['url'];
 					//console.log("picture " + i);
 					//console.log('lat: ' + data.pictures[i].lat);
 					//console.log('lng: ' + data.pictures[i].lng);
 					//console.log('url: ' + data.pictures[i].url);
 					//console.log('final url: '+ picture.url);
+					console.log("pic: " + i);
+					console.log(data.pictures[i].lat);
 					pictures.push(picture);
 				}
 				return;
@@ -108,13 +112,11 @@ angular.module('wonsoonApp')
 			async: false,
 			crossDomain: true,
 			success: function(data) {
-				if(data.route.length != 0) {
+				if(data.route &&  data.route.length != 0) {
 					for(var i = 0; i < data.route.length; i++) {
-						route_positions.push(new daum.maps.LatLng(data.route[i].lat, data.route[i].lng));
+						console.log("lat: " + i + " - " + data.route[i].lat);
+						route_positions.push(new nhn.api.map.LatLng(data.route[i].lat, data.route[i].lng));
 					}
-					center = route_positions[route_positions.length -1];
-				} else {
-					center = new daum.maps.LatLng('37.510965233668685', '127.05065575428307');
 				}
 				return;
 			},
@@ -123,43 +125,82 @@ angular.module('wonsoonApp')
 			}
 		});
 
-		// draw default map
-		var map = new daum.maps.Map(document.getElementById('map'), {
-			center: center,
-			level: 9
+		var oSeoulCityPoint = route_positions[route_positions.length-1];
+		var defaultLevel = 8;
+		var oMap = new nhn.api.map.Map(document.getElementById('map'), {
+			point : oSeoulCityPoint,
+			zoom : defaultLevel,
+			enableWheelZoom : true,
+			enableDragPan : true,
+			enableDblClickZoom : false,
+			mapMode : 0,
+			activateTrafficMap : false,
+			activateBicycleMap : false,
+			minMaxLevel : [ 1, 14 ],
+			size : new nhn.api.map.Size(1250, 670)});
+		var oSlider = new nhn.api.map.ZoomControl();
+		oMap.addControl(oSlider);
+		oSlider.setPosition({
+			top : 10,
+			left : 10
 		});
-		var mapTypeControl = new daum.maps.MapTypeControl();
-		map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
-		var mapZoomControl = new daum.maps.ZoomControl();
-		map.addControl(mapZoomControl, daum.maps.ControlPosition.RIGHT);
+		var oMapTypeBtn = new nhn.api.map.MapTypeBtn();
+		oMap.addControl(oMapTypeBtn);
+		oMapTypeBtn.setPosition({
+			bottom: 10,
+			right: 80
+		});
 
-		// draw current location marker
-		currentMarker = new daum.maps.Marker({position: route_positions[route_positions.length - 1], image: icon_wonsoon});
-		currentMarker.setMap(map);
+		var mapInfoWindow = new nhn.api.map.InfoWindow();
+		mapInfoWindow.setVisible(false);
+		oMap.addOverlay(mapInfoWindow);
 
-		// draw start locaiton marker
-		var startMarker = new daum.maps.Marker({position: route_positions[0], image: icon_start});
-		startMarker.setMap(map);
+		var oLabel = new nhn.api.map.MarkerLabel();
+		oMap.addOverlay(oLabel);
 
-		// draw route line
-		var line = new daum.maps.Polyline();
-		line.setPath(route_positions);
-		line.setMap(map);
+		mapInfoWindow.attach('changeVisible', function(oCustomEvent) {
+			if(oCustomEvent.visible) {
+				oLabel.setVisible(false);
+			}
+		});
+
+		oMap.attach('mouseenter', function(oCustomEvent) {
+			var oTarget = oCustomEvent.target;
+
+			if(oTarget instanceof nhn.api.map.Marker) {
+				var oMarker = oTarget;
+				oLabel.setVisible(true, oMarker);
+			}
+		});
+
+		oMap.attach('mouseleave', function(oCustomEvent) {
+			var oTarget = oCustomEvent.target;
+			if (oTarget instanceof nhn.api.map.Marker) {
+				oLabel.setVisible(false);
+			}
+		});
+
+		if(route_positions.length != 0) {
+			currentMarker = new nhn.api.map.Marker(icon_wonsoon);
+			currentMarker.setPoint(route_positions[route_positions.length - 1]);
+			oMap.addOverlay(currentMarker);
 		
-		// draw pictures_markers and show infoWindow for each pictures_markers
-		for(var i= 0; i < pictures.length; i++) {
-			pictures_markers[i] = new daum.maps.Marker({position: pictures[i].position, image: icon});
-			pictures_markers[i].pictures_url = "<img src='" + pictures[i].url + "' style='width: 250px;' />";
-			pictures_markers[i].info = new daum.maps.InfoWindow();
-			daum.maps.event.addListener(pictures_markers[i], 'mouseover', function() {
-				this.info.setContent(this.pictures_url);
-				this.info.open(map, this);
-				this.setImage(icon_click);
+			startMarker = new nhn.api.map.Marker(icon_start);
+			startMarker.setPoint(route_positions[0]);
+			oMap.addOverlay(startMarker);
+			
+			oPolyLine = new nhn.api.map.Polyline(route_positions, {
+				strokeColor: '#f00',
+				strokeWidth: 5,
+				strokeOpacity: 0.5
 			});
-			daum.maps.event.addListener(pictures_markers[i], 'mouseout', function() {
-				this.info.close();
-				this.setImage(icon);
-			});
-			pictures_markers[i].setMap(map);
+			oMap.addOverlay(oPolyLine);
+		}
+
+		// show markers
+		for ( var i = 0; i< pictures.length; i++) {
+			pictures_markers[i] = new nhn.api.map.Marker(icon, {title: "<img src='" + pictures[i].url + "' style='width:200px; height:300px;' class='pics'/>"});
+			pictures_markers[i].setPoint(pictures[i].position);
+			oMap.addOverlay(pictures_markers[i]);
 		}
 });
